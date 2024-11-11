06371b70d12b95740c2379fdda3f1cf71fa9d193
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-11-11 18:12:28 +0530

    beautified header dummy

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
 
 const Header = () => {
-  return (
-    <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
-      <h1 className="text-2xl font-bold ml-10">DebateVerse</h1>
-      <div>
-        <SignedIn>
-          <UserButton/>
-        </SignedIn>
-      </div>
+return (
+    <header className="flex justify-between items-center p-4 text-white">
+            <div className='flex flex-row'>
+                    <Image src="/assets/logo.png" alt="Logo" width={100} height={100} />
+                    {/* <Image src="/assets/logo-text.png" className='pt-5 ml-[-10px]' alt="Logo" width={500} height={60} /> */}
+                    <h1>DebateVerse</h1>
+            </div>
+            <div>
+                    <SignedIn>
+                    <UserButton/>
+                    </SignedIn>
+            </div>
     </header>
-  );
+);
 };
 
 export default Header;
