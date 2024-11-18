14755c3935ed73399fd51ca354c8ee91fc87e9d5
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-11-18 17:02:45 +0530

    deleted fonts in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,17 +10,6 @@ import ConditionalHeader from '@/actions/ConditionalHeader';
 import './globals.css';
 import RoleRedirect from "@/actions/RoleRedirect";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata = {
   title: "DebateVerse",
   description: "A platform for debating and discussing ideas.",
